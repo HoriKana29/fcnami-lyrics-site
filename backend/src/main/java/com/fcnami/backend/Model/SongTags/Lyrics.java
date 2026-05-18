4144a9f0c88cf2637d7eefd3f476f8826cb615e3
HoriKana29 <arialfuaral36118@gmail.com>
2026-05-18 18:18:44 +0700

    Add for InteliJ

--- backend/src/main/java/com/fcnami/backend/Model/SongTags/Lyrics.java
+++ backend/src/main/java/com/fcnami/backend/Model/SongTags/Lyrics.java
@@ -8,10 +8,17 @@ import java.time.LocalDateTime;
 
 @Getter
 @Setter
+// Class Lyrics เป็น Entity ของ JPA ( Java Persistence API )
+// ** Entity : Class ที่เป็นตัวแทนของ Table ใน Database ในรูปแบบ Object
+// ใช้เก็บข้อมูล "เนื้อเพลง" ของแต่ละเพลง โดยแยกเป็นหลายรูปแบบ (kanji, romaji, thai, notes)
 @Entity
 @Table(name = "lyrics")
 public class Lyrics {
+
+    // @Id เป็น Primary Key เหมือนรหัสของแต่ละแถวในตาราง
+    // คุณสมบัติ : ไม่ซ้ำ/ not null / one per table
     @Id
+    // ให้ Primary Key เพิ่มค่าเองอัตโนมัติ
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
